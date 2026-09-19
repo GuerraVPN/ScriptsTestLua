2cfb23fab81bd22022db49c7cc50c85594f5ae56
GuerraVPN <81371258+GuerraVPN@users.noreply.github.com>
2026-09-19 15:00:17 -0300

    Orbis Vault v0.3: update orbisvault/app/src/main/java/com/orbisvault/admin/MainActivity.java

--- orbisvault/app/src/main/java/com/orbisvault/admin/MainActivity.java
+++ orbisvault/app/src/main/java/com/orbisvault/admin/MainActivity.java
@@ -2,21 +2,28 @@ package com.orbisvault.admin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.graphics.Color;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.graphics.Color;
 import android.view.Window;
 
 public class MainActivity extends Activity {
     private WebView webView;
+    private ValueCallback<Uri[]> filePathCallback;
+    private static final int FILE_CHOOSER_REQUEST = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Window w = getWindow();
-        w.setStatusBarColor(Color.rgb(2,8,23));
-        w.setNavigationBarColor(Color.rgb(2,8,23));
+        w.setStatusBarColor(Color.rgb(2, 8, 23));
+        w.setNavigationBarColor(Color.rgb(2, 8, 23));
 
         webView = new WebView(this);
         setContentView(webView);
@@ -27,18 +34,69 @@ public class MainActivity extends Activity {
         s.setDatabaseEnabled(true);
         s.setAllowFileAccess(true);
         s.setAllowContentAccess(true);
+        s.setAllowFileAccessFromFileURLs(true);
+        s.setAllowUniversalAccessFromFileURLs(true);
         s.setBuiltInZoomControls(false);
         s.setDisplayZoomControls(false);
         s.setMediaPlaybackRequiresUserGesture(false);
 
-        webView.setBackgroundColor(Color.rgb(2,8,23));
+        webView.setBackgroundColor(Color.rgb(2, 8, 23));
         webView.setWebViewClient(new WebViewClient());
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(
+                    WebView webView,
+                    ValueCallback<Uri[]> filePath,
+                    FileChooserParams fileChooserParams) {
+
+                if (filePathCallback != null) {
+                    filePathCallback.onReceiveValue(null);
+                }
+                filePathCallback = filePath;
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+
+                try {
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Selecionar capa"),
+                            FILE_CHOOSER_REQUEST
+                    );
+                } catch (Exception e) {
+                    filePathCallback = null;
+                    return false;
+                }
+                return true;
+            }
+        });
+
         webView.loadUrl("file:///android_asset/index.html");
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILE_CHOOSER_REQUEST) {
+            if (filePathCallback != null) {
+                Uri[] result = null;
+                if (resultCode == RESULT_OK && data != null && data.getData() != null) {
+                    result = new Uri[]{data.getData()};
+                }
+                filePathCallback.onReceiveValue(result);
+                filePathCallback = null;
+            }
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
     public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
