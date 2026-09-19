5db63beb00e3cb9fffd102349f481dc2fb82b06a
GuerraVPN <81371258+GuerraVPN@users.noreply.github.com>
2026-09-19 12:36:52 -0300

    Orbis Vault: add orbisvault/app/src/main/java/com/orbisvault/admin/MainActivity.java

---
+++ orbisvault/app/src/main/java/com/orbisvault/admin/MainActivity.java
@@ -0,0 +1,44 @@
+package com.orbisvault.admin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.graphics.Color;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window w = getWindow();
+        w.setStatusBarColor(Color.rgb(2,8,23));
+        w.setNavigationBarColor(Color.rgb(2,8,23));
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        s.setBuiltInZoomControls(false);
+        s.setDisplayZoomControls(false);
+        s.setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setBackgroundColor(Color.rgb(2,8,23));
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
